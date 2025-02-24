2c78ea49a60b6a93048da29ba740250934e69776
Alex Worden <alexworden@gmail.com>
2025-02-23 18:17:49 -0800

    fix: remove hardcoded port defaults in backend config and env

--- backend/src/app/config.ts
+++ backend/src/app/config.ts
@@ -1,10 +1,10 @@
 export const config = {
   webUrl: process.env.NODE_ENV === 'production'
     ? `https://${process.env.RTR_WEB_HOST}`
-    : `${process.env.RTR_WEB_PROTOCOL || 'http'}://${process.env.RTR_WEB_HOST || 'localhost'}:${process.env.RTR_WEB_PORT || '4200'}`,
+    : `${process.env.RTR_WEB_PROTOCOL || 'http'}://${process.env.RTR_WEB_HOST || 'localhost'}:${process.env.RTR_WEB_PORT || ''}`,
   apiUrl: process.env.NODE_ENV === 'production'
     ? `${process.env.RTR_API_PROTOCOL || 'http'}://${process.env.RTR_API_HOST || 'localhost'}`
-    : `${process.env.RTR_API_PROTOCOL || 'http'}://${process.env.RTR_API_HOST || 'localhost'}:${process.env.RTR_API_PORT || '3000'}`,
+    : `${process.env.RTR_API_PROTOCOL || 'http'}://${process.env.RTR_API_HOST || 'localhost'}:${process.env.RTR_API_PORT || ''}`,
   apiHost: process.env.RTR_API_HOST || 'localhost',
   apiPort: process.env.NODE_ENV === 'production'
     ? parseInt(process.env.PORT || '80')
